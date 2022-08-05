2b9db204fae37f5c3449d31969f2e461a1f4b6fa
Filipe Cardoso <filipecardosooliveira@hotmail.com>
2022-08-05 11:37:02 -0300

    remove text error

--- src/main/java/com/example/MARS/controllers/MarsController.java
+++ src/main/java/com/example/MARS/controllers/MarsController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestControllercurl -s --request POST http://localhost:8080/rest/mars/MML
+@RestController
 @RequestMapping("/rest")
 public class MarsController {
     @Autowired
