eafb3e062607c5279abad9c77b0f436981eab193
Filipe Cardoso <filipecardosooliveira@hotmail.com>
2022-08-07 14:37:46 -0300

    mod: change execute() function from MarsCommands to void

--- src/main/java/com/example/MARS/enums/MarsCommands.java
+++ src/main/java/com/example/MARS/enums/MarsCommands.java
@@ -4,23 +4,20 @@ import com.example.MARS.utils.MarsPosition;
 
 public enum MarsCommands {
     M{
-        public MarsPosition execute( MarsPosition marsPosition){
+        public void execute(MarsPosition marsPosition){
             marsPosition.walk();
-            return marsPosition;
         }
     },
     L{
-        public MarsPosition execute( MarsPosition marsPosition){
+        public void execute(MarsPosition marsPosition){
              marsPosition.toLeft();
-            return marsPosition;
         }
     },
     R{
-        public MarsPosition execute( MarsPosition marsPosition){
+        public void execute(MarsPosition marsPosition){
             marsPosition.toRight();
-            return marsPosition;
         }
     };
 
-    public abstract MarsPosition execute(MarsPosition marsPosition);
+    public abstract void execute(MarsPosition marsPosition);
 }
